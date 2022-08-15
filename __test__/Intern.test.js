6a05a31596f9f9c04d03d715fd1fbdf9f023ea54
MiguelJerome <sharkmaycry@gmail.com>
2022-08-15 17:44:21 -0400

    final testing html for video

--- __test__/Intern.test.js
+++ __test__/Intern.test.js
@@ -2,10 +2,10 @@
 const Intern = require('../lib/Intern.js');
 
 test('creates a intern object', () => {
-  const intern = new Intern('Miguel',3,'2001326@collegelacite.com', 5);
+  const intern = new Intern('Miguel',3,'2001326@collegelacite.com', 'Ottawa');
 
   expect(intern.name).toBe('Miguel');
   expect(intern.id).toEqual(expect.any(Number));
   expect(intern.email ).toBe('2001326@collegelacite.com');
-  expect(intern.school).toEqual(expect.any(Number));
+  expect(intern.school).toBe('Ottawa');
 });
